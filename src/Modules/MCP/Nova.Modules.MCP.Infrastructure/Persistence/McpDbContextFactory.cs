dd54b0a6e521bb7e078ad97499c522fbe78b0a42
LYP <14142744+lyp_conding@user.noreply.gitee.com>
2026-09-16 09:40:20 +0800

    fix:补充McpDbContextFactory.cs文件

---
+++ src/Modules/MCP/Nova.Modules.MCP.Infrastructure/Persistence/McpDbContextFactory.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Nova.Framework.MultiTenancy;
+using Nova.Framework.MultiTenancy.EntityFrameworkCore;
+using Nova.Modules.Mcp.Infrastructure.Persistence;
+
+namespace Nova.Modules.MCP.Infrastructure.Persistence
+{
+    public class McpDbContextFactory : DesignTimeDbContextFactoryBase<McpDbContext>
+    {
+        protected override McpDbContext CreateDbContext(DbContextOptions<McpDbContext> options)
+        {
+            var dummyTenant = new NovaTenantInfo { Id = "dummy", Identifier = "dummy" };
+            return new McpDbContext(dummyTenant, options);
+        }
+    }
+}
